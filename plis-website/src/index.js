45b82007ec9a60dcb9ea0da3ae8282831b89e6da
Irmak Kavasoglu <irmakkavasoglu@gmail.com>
2019-01-05 20:04:42 +0300

    Add boun logo and product name in fullscreen page

--- plis-website/src/index.js
+++ plis-website/src/index.js
@@ -2,9 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Search extends React.Component {
+class FullscreenSearch extends React.Component {
+    render() {
+        return (
+            <div className="vertical-flex">
+                <img width="200px" height="200px" src="/boun_logo.png" alt=""/>
+                <p>Protein Ligand Interaction Search</p>
+            </div>
+        );    
+    }
+}
+
+/**
+ * Holds the query in its state.
+ * Decides between fulscreen design and results design.
+ */
+class Plis extends React.Component {
     constructor(props) {
         super(props);
+        // Initially the query is empty.
         this.state = {
             query: "",
         };
@@ -14,12 +30,12 @@ class Search extends React.Component {
         let currentQuery = this.state.query.trim();
         // If current query is empty, show the fullscreen search.
         if (currentQuery === "") {
+            // Query is empty. Go for the fullscreen design.
             return (
-                <div>
-                    fullscreen search
-                </div>
-            );    
+                <FullscreenSearch />
+            );
         } else {
+            // Query there is a query, go for results page.
             return (
                 <div>
                     results search
@@ -30,4 +46,4 @@ class Search extends React.Component {
     }
 }
 
-ReactDOM.render(<Search />, document.getElementById('root'));
+ReactDOM.render(<Plis />, document.getElementById('root'));
