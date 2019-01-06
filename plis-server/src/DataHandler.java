c5f3033b6e7798a65839de2fccc8119369138d82
Irmak Kavasoglu <irmakkavasoglu@gmail.com>
2019-01-06 15:06:05 +0300

    Added kiba files to server

---
+++ plis-server/src/DataHandler.java
@@ -0,0 +1,8 @@
+public class DataHandler {
+    /**
+     * Import data from kiba dataset.
+     */
+    public static void prepareCoreDataset() {
+        // TODO: Import.
+    }
+}
--- plis-server/src/Main.java
+++ plis-server/src/Main.java
@@ -4,6 +4,10 @@ import java.net.InetSocketAddress;
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        // Prepare data.
+        DataHandler.prepareCoreDataset();
+
+        // Create and start server.
         HttpServer server = HttpServer.create(new InetSocketAddress(60015), 0);
         server.createContext("/", new ConnectionHandler());
         server.setExecutor(null); // creates a default executor
