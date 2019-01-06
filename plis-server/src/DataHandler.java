50cc94de77586a5caabf06718efc3aa26b6caa1f
Irmak Kavasoglu <irmakkavasoglu@gmail.com>
2019-01-06 15:12:39 +0300

    Import kiba files to server

--- plis-server/src/DataHandler.java
+++ plis-server/src/DataHandler.java
@@ -1,8 +1,71 @@
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class DataHandler {
+    static JSONObject kibaLigands, kibaProteins;
+    static String kibaAffinities[][];
+
     /**
      * Import data from kiba dataset.
      */
     public static void prepareCoreDataset() {
-        // TODO: Import.
+        System.out.println("Importing core dataset.");
+        importLigands();
+        importProteins();
+        importAffinities();
+    }
+
+    private static void importLigands() {
+        System.out.println("Reading the ligand database.");
+        Scanner in = null;
+        try {
+            in = new Scanner(new File("files/kiba/kiba_ligands.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        String ligandsDB = in.nextLine();
+        kibaLigands = new JSONObject(ligandsDB);
+
+        System.out.println("Imported " + kibaLigands.keySet().size() + " kibaLigands from core dataset.");
+    }
+
+    private static void importProteins() {
+        System.out.println("Reading the protein database.");
+        Scanner in = null;
+        try {
+            in = new Scanner(new File("files/kiba/kiba_proteins.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        String proteinsDB = in.nextLine();
+        kibaProteins = new JSONObject(proteinsDB);
+
+        System.out.println("Imported " + kibaProteins.keySet().size() + " kibaProteins from core dataset.");
+    }
+
+    private static void importAffinities() {
+        kibaAffinities = new String[kibaLigands.keySet().size()][kibaProteins.keySet().size()];
+
+        System.out.println("Reading the binding affinity database.");
+        Scanner in = null;
+        try {
+            in = new Scanner(new File("files/kiba/kiba_binding_affinity_v2.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        for (int i = 0; i < kibaAffinities.length; i++) {
+            String line = in.nextLine();
+            String[] cols = line.split("\t");
+            for (int j = 0; j < cols.length; j++) {
+                kibaAffinities[i][j] = cols[j];
+            }
+        }
+        System.out.println("Imported " + kibaAffinities.length * kibaAffinities[0].length + " kibaAffinities from core dataset.");
     }
 }
